0033e9c0af6cf79e5b75dd51f0b307462d60d840
rootspyro <root.spyro@gmail.com>
2022-08-31 23:11:37 -0400

    provisional domain cors

--- src/lambda.ts
+++ src/lambda.ts
@@ -18,19 +18,8 @@ async function bootstrap() {
   const whitelist = ['https://rootspyro.com', 'https://portfolio-ui-ten.vercel.app'];
 
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log("allowed cors for:", origin)
-        callback(null, true)
-      } else {
-        console.log("blocked cors for:", origin)
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-    credentials: true,
-  });
+    origin : 'https://portfolio-ui-ten.vercel.app'
+  })
 
   //SWAGGER
   const options = new DocumentBuilder()
